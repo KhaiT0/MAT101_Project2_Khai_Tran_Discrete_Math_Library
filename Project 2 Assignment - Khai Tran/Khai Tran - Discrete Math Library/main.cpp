e3bc4b1c85bc2cc34a4ce70998f84737b49e8c44
Khai Tran <Khai.Tran@mds.ac.nz>
2026-06-16 03:35:50 +1200

    Did main.cpp & test.cpp
    Just copied what Alexa did in the workshop as I'm 200% confused on what's going on right now! But I assume it's to create a working test to see if our code works

--- Project 2 Assignment - Khai Tran/Khai Tran - Discrete Math Library/main.cpp
+++ Project 2 Assignment - Khai Tran/Khai Tran - Discrete Math Library/main.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+#include "discretemaths.h"
+#include "test.h"
+
+#define PI 3.141592653589793f
+
+float RadsToDegs(float _Rads);
+
+int main()
+{
+	TestEquals();
+
+	return 0;
+}
+
+float RadsToDegs(float _Rads)
+{
+	return _Rads * 180 / PI;
+}
--- Project 2 Assignment - Khai Tran/Khai Tran - Discrete Math Library/test.cpp
+++ Project 2 Assignment - Khai Tran/Khai Tran - Discrete Math Library/test.cpp
@@ -1,2 +1,68 @@
 #include "test.h"
 #include <iostream>
+
+bool Equals(float _f1, float _f2, float _precision)
+{
+	return (_f1 < _f2 + _precision) && (_f1 > _f2 - _precision);
+}
+
+bool TestExpectedState(float _output, float _expected)
+{
+	return Equals(_output, _expected, 0.001f);
+}
+
+bool TestEquals()
+{
+	bool PassedAllTests = true;
+	if (!Equals(0.0f, 0.0f, 0.001f))
+	{
+		std::cout << "Failed Test: 0.0 | 0.0: Expected TRUE" << std::endl;
+		PassedAllTests = false;
+	}
+	else
+	{
+		std::cout << "Succeeded Test: 0.0 | 0.0: Expected TRUE" << std::endl;
+	}
+
+	if (!Equals(1.0f, 0.0f, 0.1f))
+	{
+		std::cout << "Failed Test: 1.0 | 0.0: Expected FALSE" << std::endl;
+		PassedAllTests = false;
+	}
+	else
+	{
+		std::cout << "Succeeded Test: 1.0 | 0.0: Expected FALSE" << std::endl;
+	}
+
+	if (!Equals(0.0f, 1.0f, 0.1f))
+	{
+		std::cout << "Failed Test: 0.0 | 1.0: Expected FALSE" << std::endl;
+		PassedAllTests = false;
+	}
+	else
+	{
+		std::cout << "Succeeded Test: 0.0 | 1.0: Expected FALSE" << std::endl;
+	}
+
+// if (!Equals(-2.0f, 0.08f, 0.1f))
+// {
+//		std::cout << "Failed Test: -2.0 | 0.08: Expected TRUE" << std::endl;
+//		PassAllTests = false;
+//	}
+//	else
+//	{
+//		std::cout << "Succeeded Test: -2.0 | 0.08: Expected TRUE" << std::endl;
+//	}
+//
+//	if (!Equals(20.0f, 20.1f, 0.1f))
+//	{
+//		std::cout << "Failed Test: 20.0 | 20.1: Expected TRUE" << std::endl;
+//		PassAllTests = false;
+//	}
+//	else
+//	{
+//		std::cout << "Succeeded Test: 20.0 | 20.1: Expected TRUE" << std::endl;
+//	}
+
+	return PassedAllTests;
+}
